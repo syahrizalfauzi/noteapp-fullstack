4e1cccb58857352fdf884e2cdf1669313546d5e8
Syahrizal Fauzi <syahrizalfauzi16@gmail.com>
2021-02-23 14:30:23 +0700

    Fixed user route

--- models/noteModel.js
+++ models/noteModel.js
@@ -1,6 +1,7 @@
 const connection = require("../db.js");
 
 const getNoteByUserId = (userId, callback) => {
+  connection.connect();
   connection.query(
     "SELECT id, title, content, time FROM notes WHERE userId = ? ORDER BY time DESC",
     userId,
@@ -9,6 +10,7 @@ const getNoteByUserId = (userId, callback) => {
 };
 
 const createNote = (title, content, userId, callback) => {
+  connection.connect();
   connection.query(
     "INSERT INTO notes (title, content, userId) VALUES (?,?,?)",
     [title, content, userId],
@@ -17,6 +19,7 @@ const createNote = (title, content, userId, callback) => {
 };
 
 const updateNote = (title, content, noteId, userId, callback) => {
+  connection.connect();
   connection.query(
     "UPDATE notes SET title = ?, content = ?, time = CURRENT_TIMESTAMP WHERE id = ? AND userid = ?",
     [title, content, noteId, userId],
@@ -25,6 +28,7 @@ const updateNote = (title, content, noteId, userId, callback) => {
 };
 
 const deleteNote = (noteId, userId, callback) => {
+  connection.connect();
   connection.query(
     "DELETE FROM notes WHERE id = ? AND userid = ?",
     [noteId, userId],
